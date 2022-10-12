d864a59da39285bb1f7f81cea06652bb969a2629
M Muchsin <87637609+mmuchsin@users.noreply.github.com>
2022-10-12 08:06:57 +0700

    implementing pathlib

--- src/preprocessing/data_loader.py
+++ src/preprocessing/data_loader.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import pathlib
+
+DATA_DIR = pathlib.Path.cwd().parent.joinpath("data")
 
 def get_literasi():
-    literasi = pd.read_excel("D:/Tetris 2022/capstone project/digital_literasi_hoax/dashboard/src/data/indeks-literasi-digital-indonesia.xlsx")
+    literasi = pd.read_excel(DATA_DIR.joinpath("indeks-literasi-digital-indonesia.xlsx"))
     return literasi
 
 def get_kominfo():
-    kominfo = pd.read_csv("D:/Tetris 2022/capstone project/digital_literasi_hoax/dashboard/src/data/data_hoax_2020_2021_kominfo.csv", parse_dates=["date"])
+    kominfo = pd.read_csv(DATA_DIR.joinpath("data_hoax_2020_2021_kominfo.csv", parse_dates=["date"]))
     return kominfo
 
 def get_turnbackoax():
-    turnbackoax = pd.read_csv("D:/Tetris 2022/capstone project/digital_literasi_hoax/dashboard/src/data/turnback2020-2021")
+    turnbackoax = pd.read_csv(DATA_DIR.joinpath("turnback2020-2021"))
     return turnbackoax
