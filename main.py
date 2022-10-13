03d2970120959bfabb86024aee9410d390fa82fc
M Muchsin <87637609+mmuchsin@users.noreply.github.com>
2022-10-13 10:09:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -101,8 +101,9 @@ col1, col2 = st.columns(2, gap="large")
 
 st.markdown("""<div style='text-align: center'> Tabel Indeks Literasi Digital
                 dan Jumlah Hoaks Per Tahun</div>""", unsafe_allow_html=True)
-    corr_data = data_cleaner.get_corr_data(literasi, kominfo_tahunan)
-    st.dataframe(corr_data, use_container_width=True)
+
+corr_data = data_cleaner.get_corr_data(literasi, kominfo_tahunan)
+st.dataframe(corr_data, use_container_width=True)
 
 st.write(
         """
