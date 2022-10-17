95ecd530ae829aada536b909cb80bd517535a97a
mmuchsin <87637609+mmuchsin@users.noreply.github.com>
2022-10-17 23:48:39 +0000

    update isi hoaks

--- src/preprocessor/data_loader.py
+++ src/preprocessor/data_loader.py
@@ -22,58 +22,87 @@ def get_combine():
     combine = pd.read_csv(DATA_DIR.joinpath("combine.csv"))
     return combine
 
-def get_penyebaran_hoaks():
+
+def get_penyebaran_hoaks20():
     data = {
         "media": [
-            "sosial media",
-            "aplikasi chatting",
-            "website",
-            "media cetak",
-            "email",
-            "tv"
+            "facebook",
+            "whatsapp",
+            "youtube",
+            "tv",
+            "portal berita online",
+            "instagram",
+            "twitter",
+            "radio",
+            "koran/majalah",
+            "line"
+            "tidak ada/tidak tahu"
         ],
-        "persentase": [87.5, 67, 28.2, 6.4, 2.6, 8.1]
+        "persentase": [71.9, 31.5, 14.9, 7.7, 10.7, 8.1, 1.9, 0.5, 2.5, 0.4, 2.9],
+        "tahun": [2020, 2020, 2020, 2020, 2020, 2020, 2020, 2020, 2020, 2020, 2020]
     }
     df = pd.DataFrame(data)
     return df
 
-def get_bentuk_hoaks():
+def get_penyebaran_hoaks21():
     data = {
-        "bentuk": [
-            "tulisan",
-            "foto editan",
-            "foto caption palsu",
-            "video dubbing palsu",
-            "video dipotong",
-            "video caption palsu",
-            "repost"
+        "media": [
+            "facebook",
+            "whatsapp",
+            "youtube",
+            "tv",
+            "portal berita online",
+            "instagram",
+            "twitter",
+            "radio",
+            "koran/majalah",
+            "line"
+            "tidak ada/tidak tahu"
         ],
-        "persentase": [70.7, 57.8, 66.3, 33.2, 45.7, 53.2, 69.2]
+        "persentase": [62.6, 20.5, 16.4, 16.3, 14.9, 9.7, 2.7, 1.5, 1.3, 0.7, 1.1],
+        "tahun": [2021, 2021, 2021, 2021, 2021, 2021, 2021, 2021, 2021, 2021, 2021]
     }
     df = pd.DataFrame(data)
     return df
 
 
-def get_isi_hoaks():
+def get_isi_hoaks20():
+    data = {
+       "topik": [
+            "politik",
+            "kesehatan",
+            "agama",
+            "lingkungan",
+            "kerusuhan",
+            "bencana alam",
+            "tidak tahu",
+            "lainnya",
+        ],
+        "persentase": [67.2, 46.3, 33.2, 21.9, 28.1, 12.4, 2.3, 1.4],
+        "tahun": [2020, 2020, 2020, 2020, 2020, 2020, 2020, 2020]
+    }
+    df = pd.DataFrame(data)
+    return df
+
+
+def get_isi_hoaks21():
     data = {
         "topik": [
-            "info pekerjaan",
-            "kecelakaan lalu lintas",
-            "bencana alam",
-            "sosial budaya",
-            "berita duka",
-            "iptek",
-            "penipuan keuangan",
-            "makanan & minumam",
+            "politik",
             "kesehatan",
-            "pemerintahan",
-            "sara",
-            "sosial politik"
+            "agama",
+            "lingkungan",
+            "kerusuhan",
+            "bencana alam",
+            "tidak tahu",
+            "lainnya",
         ],
-        "persentase": [24.4, 13.5, 29.3, 18.1, 16.8, 20, 18.5, 30, 40.7, 61.7, 76.2, 93.2]
+        "persentase": [69.3, 39.7, 29.2, 21.4, 13.4, 10.9, 0, 0],
+        "tahun": [2021, 2021, 2021, 2021, 2021, 2021, 2021, 2021]
     }
     df = pd.DataFrame(data)
     return df
+
 
 def get_literasi_prov20():
     df = pd.read_excel(
