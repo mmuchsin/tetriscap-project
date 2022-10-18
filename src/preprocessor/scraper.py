ecd654fbf0627ab64e48f863a551880d20c6c1c0
mmuchsin <87637609+mmuchsin@users.noreply.github.com>
2022-10-18 03:09:44 +0000

    update scraper

---
+++ src/preprocessor/scraper.py
@@ -0,0 +1,21 @@
+from newspaper import Article
+
+def Scrap(url):
+    """Scrap article from url
+
+    Args:
+        url (`string`): news url
+
+    Returns:
+        article (`string`): news content
+    """
+
+    article = Article(url, language='id')
+    article.download()
+    article.parse()
+
+    if not article.text:
+        print("Can't Scrap this article link")
+        return None
+
+    return article
